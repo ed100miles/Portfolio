0edc433ba52140117cd80339c295bb980f6de5e5
ed100miles <ed100miles@gmail.com>
2021-02-08 22:05:27 +0000

    put scrabbler.html form in drop down

--- static/scripts.js
+++ static/scripts.js
@@ -1,15 +1,16 @@
 console.log('JS loaded okay')
 
 // matches div heights in scrabble page
-
-function div_match(){
-    console.log('matching divs')
-    var scrabbleFormHeight = document.getElementById('scrabbleForm').clientHeight;
-    var scrabbleWordsHeight = Number(document.getElementById('scrabbleWords').clientHeight);
-    if (scrabbleFormHeight != scrabbleWordsHeight){
-        var newScrabbleWordsHeight = scrabbleFormHeight + "px";
-        document.getElementById('scrabbleWords').style.height=newScrabbleWordsHeight;
-        
-    }
-}
-
+// 
+// function div_match(){
+    // console.log('matching divs')
+    // var scrabbleFormHeight = document.getElementById('scrabbleForm').clientHeight;
+    // var scrabbleWordsHeight = Number(document.getElementById('scrabbleWords').clientHeight);
+    // if (scrabbleFormHeight != scrabbleWordsHeight){
+        // var newScrabbleWordsHeight = scrabbleFormHeight + "px";
+        // document.getElementById('scrabbleWords').style.height=newScrabbleWordsHeight;
+        // 
+    // }
+// }
+// 
+// 
