10cb3d0304d3cec12d710f558c8edd6bce2b6406
ed100miles <ed100miles@gmail.com>
2021-02-10 20:11:58 +0000

    JS loadbar added for 'getwords' on scrabbler page.

--- static/scripts.js
+++ static/scripts.js
@@ -1,16 +1,11 @@
 console.log('JS loaded okay')
 
-// matches div heights in scrabble page
-// 
-// function div_match(){
-    // console.log('matching divs')
-    // var scrabbleFormHeight = document.getElementById('scrabbleForm').clientHeight;
-    // var scrabbleWordsHeight = Number(document.getElementById('scrabbleWords').clientHeight);
-    // if (scrabbleFormHeight != scrabbleWordsHeight){
-        // var newScrabbleWordsHeight = scrabbleFormHeight + "px";
-        // document.getElementById('scrabbleWords').style.height=newScrabbleWordsHeight;
-        // 
-    // }
-// }
-// 
-// 
+function loadbar(){
+    setInterval(()=> {
+        document.getElementById("wordProgressBar").value
+        = document.getElementById("wordProgressBar").value + 1;
+        if (document.getElementById("wordProgressBar").value === 100){
+            document.getElementById("wordProgressBar").value = 1
+        }
+    }, 50)
+}
